606737528c49573ba519b0370139f106e25d943e
Ben McLean <benmclean007@gmail.com>
2024-10-05 15:47:42 +1000

    Fix non-gerber pos files

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ kicad-cli pcb export drill -o "$output" "$input"
 kicad-cli pcb export gerbers -o "$output" "$input"
 
 if [[ "$placement_format" == "ascii" || "$placement_format" == "csv" ]]; then
-    kicad-cli pcb export pos --format "$placement_format" -o "$output" "$input"
+    kicad-cli pcb export pos --format "$placement_format" -o "$output/$filename.pos" "$input"
 elif [[ "$placement_format" == "gerber" ]]; then
     kicad-cli pcb export pos --format gerber --side front -o "$output/$filename.front.pos" "$input"
     kicad-cli pcb export pos --format gerber --side back -o "$output/$filename.back.pos" "$input"
